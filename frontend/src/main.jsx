b72923fa904c7b9f832f34fd4149431636d10090
Tharindu Dilanka Bandara <Dilanka.tdb@gmail.com>
2026-05-27 23:45:17 +0530

    add files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css' // 🚨 THIS IS THE MAGIC LINE! It connects Tailwind to your app.
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
