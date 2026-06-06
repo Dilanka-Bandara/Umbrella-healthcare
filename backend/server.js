f4653bd1f61c3379ff3c3f6b92380e34d72900c9
Tharindu Dilanka Bandara <Dilanka.tdb@gmail.com>
2026-06-06 21:37:34 +0530

    add store

--- backend/server.js
+++ backend/server.js
@@ -49,6 +49,7 @@ app.use('/api/patients', require('./src/routes/patientRoutes'));
 app.use('/api/doctors', require('./src/routes/doctorRoutes'));
 app.use('/api/admin', require('./src/routes/adminRoutes'));
 app.use('/api/verification', require('./src/routes/verificationRoutes'));
+app.use('/api/store', require('./src/routes/storeRoutes'));
 
 
 // Basic Route
